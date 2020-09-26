2aa59c5fa114e6c26471c3708662fbd5f7814ce0
Jose Hurtado <josehumberto17@gmail.com>
2020-09-26 18:40:06 -0500

    Agregar chrome drivers y test para nexus 5 android 7

--- src/test/java/com/company/projectg2appiume2e/base/BaseTest.java
+++ src/test/java/com/company/projectg2appiume2e/base/BaseTest.java
@@ -1,4 +1,28 @@
 package com.company.projectg2appiume2e.base;
 
+import io.appium.java_client.AppiumDriver;
+import org.junit.AfterClass;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class BaseTest {
+
+    protected static AppiumDriver driver;
+
+    public static void configAppiumDriver(DesiredCapabilities capabilities) {
+
+        try {
+            driver = new AppiumDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @AfterClass
+    public static void tearDown(){
+        driver.quit();
+    }
+
 }
--- src/test/java/com/company/projectg2appiume2e/features/AndroidEmulatorRedmineLoginWebUITest.java
+++ src/test/java/com/company/projectg2appiume2e/features/AndroidEmulatorRedmineLoginWebUITest.java
@@ -1,5 +1,33 @@
 package com.company.projectg2appiume2e.features;
 
-public class AndroidEmulatorRedmineLoginWebUITest {
+import com.company.projectg2appiume2e.base.BaseTest;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class AndroidEmulatorRedmineLoginWebUITest  extends BaseTest {
+
+
+    @Test
+    public void testLoginRedmineEmulatorNexus5Android7(){
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("automationName", "UiAutomator2");
+        cap.setCapability("platformName", "Android");
+        cap.setCapability("platformVersion", "7.0");
+        cap.setCapability("deviceName", "Nexus5Android7");
+        cap.setCapability("browserName", "Chrome");
+        cap.setCapability("chromedriverExecutableDir", "/Users/jhumbertoh/webinar/projectg2-appium-e2e/resources/drivers/chrome/mac");
+
+        cap.setCapability("avd", "Nexus5Android7");
+        cap.setCapability("avdArgs", "-port 5557");
+
+        configAppiumDriver(cap);
+
+
+
+    }
+
 
 }
