6ed7b48cce4b36c36b0f4ec72e4c533bad76486a
Jose Hurtado <josehumberto17@gmail.com>
2020-09-26 17:51:15 -0500

    Inicializar proyecto

---
+++ src/test/java/com/company/projectg2appiume2e/base/BaseTest.java
@@ -0,0 +1,4 @@
+package com.company.projectg2appiume2e.base;
+
+public class BaseTest {
+}
---
+++ src/test/java/com/company/projectg2appiume2e/features/AndroidEmulatorRedmineLoginWebUITest.java
@@ -0,0 +1,5 @@
+package com.company.projectg2appiume2e.features;
+
+public class AndroidEmulatorRedmineLoginWebUITest {
+
+}
